cf5d77240933985282bb3d10a54652039e6df676
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-04-02 19:21:25 +0530

    swapnil code

--- BuyMeWebParent/BuyMeBackEnd/src/main/java/com/buyMe/admin/security/WebSecurityConfig.java
+++ BuyMeWebParent/BuyMeBackEnd/src/main/java/com/buyMe/admin/security/WebSecurityConfig.java
@@ -50,15 +50,30 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		// TODO Auto-generated method stub
 		http.authorizeRequests()
-		.antMatchers("/users/**").hasAuthority("Admin")
-		.antMatchers("/categories/**").hasAnyAuthority("Admin","Editor")
-		.anyRequest()
-		.authenticated()
+		.antMatchers("/users/**", "/settings/**", "/countries/**", "/states/**").hasAuthority("Admin")
+		.antMatchers("/categories/**", "/brands/**").hasAnyAuthority("Admin", "Editor")
+		
+		.antMatchers("/products/new", "/products/delete/**").hasAnyAuthority("Admin", "Editor")
+		
+		.antMatchers("/products/edit/**", "/products/save", "/products/check_unique")
+			.hasAnyAuthority("Admin", "Editor", "Salesperson")
+			
+		.antMatchers("/products", "/products/", "/products/detail/**", "/products/page/**")
+			.hasAnyAuthority("Admin", "Editor", "Salesperson", "Shipper")
+			
+		.antMatchers("/products/**").hasAnyAuthority("Admin", "Editor")
+		
+		.anyRequest().authenticated()
 		.and()
-		.formLogin()
-		.loginPage("/login")
-		.usernameParameter("email").permitAll() 
-		.and().logout().permitAll().and().rememberMe().key("rem-me-key").rememberMeParameter("remember");
+		.formLogin()			
+			.loginPage("/login")
+			.usernameParameter("email")
+			.permitAll()
+		.and().logout().permitAll()
+		.and()
+			.rememberMe()
+				.key("AbcDefgHijKlmnOpqrs_1234567890")
+				.tokenValiditySeconds(7 * 24 * 60 * 60);
 		
 		
 	}
--- BuyMeWebParent/BuyMeBackEnd/src/main/resources/static/js/common.js
+++ BuyMeWebParent/BuyMeBackEnd/src/main/resources/static/js/common.js
@@ -1,22 +1,37 @@
-$(document).ready(function(){
-			$("#logoutlink").on("click",function(e){
-				e.preventDefault();
-				document.logoutForm.submit();
-			});
-			customDropdownMenu();
-		});
+$(document).ready(function() {
+	$("#logoutLink").on("click", function(e) {
+		e.preventDefault();
+		document.logoutForm.submit();
+	});
+	
+	customizeDropDownMenu();
+	customizeTabs();
+});
 
-function customDropdownMenu(){
+function customizeDropDownMenu() {
 	$(".navbar .dropdown").hover(
-		function(){
-			$(this).find('.dropdown-menu').first().stop(true,true).delay(250).slideDown();
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
 		},
-		function(){
-			$(this).find('.dropdown-menu').first().stop(true,true).delay(100).slideUp();
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
 		}
 	);
-	$(".dropdown > a").click(function(){
-		location.href=this.href;
+	
+	$(".dropdown > a").click(function() {
+		location.href = this.href;
 	});
-}		
-		
+}
+
+function customizeTabs() {
+	// Javascript to enable link to tab
+	var url = document.location.toString();
+	if (url.match('#')) {
+	    $('.nav-tabs a[href="#' + url.split('#')[1] + '"]').tab('show');
+	} 
+
+	// Change hash for page-reload
+	$('.nav-tabs a').on('shown.bs.tab', function (e) {
+	    window.location.hash = e.target.hash;
+	})	
+}
