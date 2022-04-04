c4194df01695aa4a60d43e062ff12c2423b95c23
rishikeshwagh17 <rishikesh.wagh1708@gmail.com>
2022-04-04 11:30:55 +0530

    images and data added

---
+++ BuyMeCommon/src/main/java/com/buyMe/common/exception/CategoryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.buyMe.common.exception;
+
+public class CategoryNotFoundException extends Exception {
+
+	public CategoryNotFoundException(String message) {
+		super(message);
+	}
+
+}
+
--- BuyMeWebParent/BuyMeFrontEnd/src/main/java/com/buyMe/MainController.java
+++ BuyMeWebParent/BuyMeFrontEnd/src/main/java/com/buyMe/MainController.java
@@ -1,14 +1,25 @@
 package com.buyMe;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.buyMe.category.CategoryService;
+import com.buyMe.common.entity.Category;
 
 @Controller
 public class MainController {
-@GetMapping("")
-public String viewHomePage() {
-	System.out.println("Sent Home Page ");
-	return "index";
-}
+
+	@Autowired private CategoryService categoryService;
+	
+	@GetMapping("")
+	public String viewHomePage(Model model) {
+		List<Category> listCategories = categoryService.listNoChildrenCategories();
+		model.addAttribute("listCategories", listCategories);
+		
+		return "index";
+	}
 }
